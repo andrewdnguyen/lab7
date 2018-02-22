edd8acf02f5b0188d34dbbd4d0511b40e9375ca8
Andrew Nguyen <andrewdnguyen64@gmail.com>
2018-02-21 23:58:13 -0800

    Testing

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$('.likeBtn').click(record);
+}
+
+function record() {
+	ga('send', 'event', 'like', 'click');
 }
